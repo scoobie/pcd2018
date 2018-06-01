f24bbc87158c412009d39d7d9b15d7ca0c35cefa
DESKTOP-7K8CIFA\rolan <rolando.a@sapo.pt>
2018-06-01 13:17:27 +0100

    new parallel

--- src/Parallel_p3.java
+++ src/Parallel_p3.java
@@ -1,38 +1,41 @@
 import java.util.Scanner;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 class Cracker implements Runnable {
+    public static final byte MIN_CHAR_VALUE = 'a';
+    public static final byte MAX_CHAR_VALUE = 'z';
+
     private final int maxLength;
     private final Result result;
     private final byte minFirstChar;
     private final byte maxFirstChar;
-    private final String expectedHash;
+    private final byte[] expectedHash;
     private final MessageDigest md;
     private byte[] buffer;
-    private int count = 0;
 
 
     /**
      * Each cracker have limited range for first letter of the world.
      * if we have 2 thread then:
-     *  thread 1 will work with words started from a..m
-     *  thread 2 will work with words started from n..z
+     * thread 1 will work with words started from a..m
+     * thread 2 will work with words started from n..z
      */
-    public Cracker(int chunkNumber, int maxLength, Result result, String expectedHash) {
+    public Cracker(int chunkNumber, int numberOfChunk, int maxLength, Result result, String expectedHash) {
         this.maxLength = maxLength;
         this.result = result;
-        this.expectedHash = expectedHash;
-        int range = Parallel_p3.MAX_CHAR_VALUE - Parallel_p3.MIN_CHAR_VALUE + 1;
-        int chunkSize = range / Parallel_p3.NUMBER_OF_THREADS;
-        this.minFirstChar = (byte) (Parallel_p3.MIN_CHAR_VALUE + chunkSize * chunkNumber);
-        if (chunkNumber == Parallel_p3.NUMBER_OF_THREADS - 1) {
-            this.maxFirstChar = Parallel_p3.MAX_CHAR_VALUE;
+        this.expectedHash = toArray(expectedHash);
+        int range = MAX_CHAR_VALUE - MIN_CHAR_VALUE + 1;
+        int chunkSize = range / numberOfChunk;
+        this.minFirstChar = (byte) (MIN_CHAR_VALUE + chunkSize * chunkNumber);
+        if (chunkNumber == numberOfChunk - 1) {
+            this.maxFirstChar = MAX_CHAR_VALUE;
         } else {
-            this.maxFirstChar = (byte) (Parallel_p3.MIN_CHAR_VALUE + chunkSize * (chunkNumber + 1) - 1);
+            this.maxFirstChar = (byte) (MIN_CHAR_VALUE + chunkSize * (chunkNumber + 1) - 1);
         }
 
-//            System.out.println("Start tread for : " + (char)minFirstChar + " .. " + (char)maxFirstChar);
+      System.out.println("Start tread for : " + (char)minFirstChar + " .. " + (char)maxFirstChar);
 
         buffer = new byte[1];
         initBuffer();
@@ -44,23 +47,32 @@ class Cracker implements Runnable {
         }
     }
 
+    private byte[] toArray(String hash) {
+        byte[] bytes = new byte[hash.length() / 2];
+        for(int i = 0; i< bytes.length; i++){
+            bytes[i] = (byte) Integer.parseInt(hash.substring(i*2,i*2 +2), 16);
+        }
+        return bytes;
+    }
+
     private void initBuffer() {
         buffer[0] = minFirstChar;
         for (int i = 1; i < buffer.length; i++) {
-            buffer[i] = Parallel_p3.MIN_CHAR_VALUE;
+            buffer[i] = MIN_CHAR_VALUE;
         }
     }
 
     @Override
     public void run() {
+        int count = 0;
         while (true) {
-            if (result.hashFound()) {
+            if (count % 100 == 0 && result.hashFound()) {
                 result.addCount(count);
                 break;
             }
-            String hash = calculateHash();
+            byte[] hash = calculateHash();
             count++;
-            if (hash.equals(expectedHash)) {
+            if (compareHash(hash, expectedHash)) {
                 result.addCount(count);
                 result.found(new String(buffer));
                 break;
@@ -71,6 +83,10 @@ class Cracker implements Runnable {
         }
     }
 
+    private boolean compareHash(byte[] hash, byte[] expectedHash) {
+        return Arrays.equals(hash, expectedHash);
+    }
+
     private boolean increment() {
         buffer[0]++;
         if (buffer[0] <= maxFirstChar) {
@@ -79,10 +95,10 @@ class Cracker implements Runnable {
         buffer[0] = minFirstChar;
         for (int i = 1; i < buffer.length; i++) {
             buffer[i]++;
-            if (buffer[i] <= Parallel_p3.MAX_CHAR_VALUE) {
+            if (buffer[i] <= MAX_CHAR_VALUE) {
                 return false;
             }
-            buffer[i] = Parallel_p3.MIN_CHAR_VALUE;
+            buffer[i] = MIN_CHAR_VALUE;
         }
         if (buffer.length < maxLength) {
             buffer = new byte[buffer.length + 1];
@@ -92,13 +108,13 @@ class Cracker implements Runnable {
         return true;
     }
 
-    private String calculateHash() {
+    private byte[] calculateHash() {
         md.reset();
         md.update(buffer);
-        return hashToString(md.digest());
+        return md.digest();
     }
 
-    private String hashToString(byte[] hash) {
+    public static String hashToString(byte[] hash) {
         StringBuilder hex = new StringBuilder(64 * 2);
         for (int i = 0; i < hash.length; i++) {
             hex.append(Integer.toString((hash[i] & 0xf0) >> 4, 16));
@@ -107,6 +123,7 @@ class Cracker implements Runnable {
         return hex.toString().toUpperCase();
     }
 }
+
 
 class Result {
     private volatile String word = null;
@@ -135,17 +152,14 @@ class Result {
 
 
 public class Parallel_p3 {
-    public static final int NUMBER_OF_THREADS = 8;
-
-    public static final byte MIN_CHAR_VALUE = 'a';
-    public static final byte MAX_CHAR_VALUE = 'z';
+    private static final int NUMBER_OF_THREADS = 4;
 
 
     public Result crack(String hash, int length) {
         Thread[] threads = new Thread[NUMBER_OF_THREADS];
         Result result = new Result();
         for (int i = 0; i < threads.length; i++) {
-            threads[i] = new Thread(new Cracker(i, length, result, hash));
+            threads[i] = new Thread(new Cracker(i, NUMBER_OF_THREADS, length, result, hash));
             threads[i].start();
         }
 
@@ -168,19 +182,14 @@ public class Parallel_p3 {
         int wordSize = scanner.nextInt();
         scanner.close();
 
-        try {
-            Parallel_p3 sc = new Parallel_p3();
+        Parallel_p3 sc = new Parallel_p3();
 
-            Result answer;
-            long start = System.nanoTime();
-            answer = sc.crack(hashWord, wordSize);
-            long end = System.nanoTime();
-            System.out.println("The word is: " + answer.getWord());
-            System.out.println("Number of words tested: " + answer.getCount());
-            System.out.println("Processing Time: " + ((end - start) / 1000000) + " Milliseconds");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        long start = System.nanoTime();
+        Result result = sc.crack(hashWord, wordSize);
+        long end = System.nanoTime();
+        System.out.println("The word is: " + result.getWord());
+        System.out.println("Number of words tested: " + result.getCount());
+        System.out.println("Processing Time: " + ((end - start) / 1000000) + " Milliseconds");
     }
 
 }
